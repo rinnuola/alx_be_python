9ceec7f38c8065280d7e2419ec1e4e7c2a2e9e77
Aderinola <aderinolaanna@gmail.com>
2025-09-12 02:57:12 +0100

    Add simple_interest.py script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+# simple_interest.py
+
+# Step 1: Define the investment details
+principal = 1000
+rate = 0.05
+time = 3
+
+# Step 2: Calculate simple interest
+interest = principal * rate * time
+
+# Step 3: Print the result
+print(f"The simple interest is: {interest}")
+# The simple interest is: 150.0 
