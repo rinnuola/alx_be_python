f798ec79de1b85cecc4b47ffa20785ee1594ddae
Aderinola <aderinolaanna@gmail.com>
2025-09-17 00:57:11 +0100

    Add match_case script

---
+++ Control_Flow_Practice/match_case.py
@@ -0,0 +1,19 @@
+# Match Case simplifies checking for specific values
+# It is similar to switch-case statements in other languages
+
+day = "saturday"  #  assign one value at a time
+match day:
+    case "monday":
+        print("Ugh, Mondays...")
+    case "tuesday":
+        print("Just another workday...")
+    case "wednesday":
+        print("Hump day!")
+    case "thursday":
+        print("Almost there...")
+    case "friday":
+        print("TGIF!")
+    case "saturday" | "sunday":  # Match multiple values with pipe (|)
+        print("Weekend vibes!")
+    case _:
+        print("Invalid day entered.")
