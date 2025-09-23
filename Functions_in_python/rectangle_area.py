7ddbfdbbd4f4e9e7c44eeaf18b36709d29efd4d5
Aderinola <aderinolaanna@gmail.com>
2025-09-24 00:29:45 +0100

    Update rectangle_area.py with new code

--- Functions_in_python/rectangle_area.py
+++ Functions_in_python/rectangle_area.py
@@ -1,1 +1,9 @@
+# Create a function to calculate the area of a rectangle.
+# The function should take the length and width as parameters and return the area.
 
+def rectangle_area(length, width):
+    """Calculate the area of the rectangle"""
+    return length * width
+#Test
+area = rectangle_area(10, 5)
+print(f"The area of the rectangle is {area}")
